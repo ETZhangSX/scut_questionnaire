5271dc782e461a4ac15a9d78ed6215bf92fec315
ETZhangSX <etzhangsx@qq.com>
2019-05-28 16:57:18 +0800

    Add package iSlider and use it

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,4 +1,3 @@
-
 // 从后台加载问卷题目
 function getQuestionnaire() {
 
@@ -9,19 +8,74 @@ var question_data = [
     {"id": 2, "type":"radio", "content": "你物理好吗？", "options":["非常好", "很好", "一般", "菜鸡"]},
     {"id": 3, "type":"radio", "content": "你化学好吗？", "options":["非常好", "很好", "一般", "菜鸡"]},
     {"id": 4, "type":"radio", "content": "你情商高吗？", "options":["非常好", "很好", "一般", "菜鸡"]},
-]
+];
+
+var question_list = [];
 
 // 根据题目数据写HTML
 function generateForm() {
-    for (var i in question_data) {
-        document.write(question_data[i].content.concat("<br>"))
-        for (var k in question_data[i].options) {
 
-            document.write("<input type=\"".concat(question_data[i].type, "\" name=\"", question_data[i].id, "\" value=\"", k, "\">", question_data[i].options[k]))
-            document.write("<br>")
-        }
+    for (var i in question_data) {
+        question_list.push({
+            content: createQuestionItem(question_data[i])
+        });
+    };
+
+    question_list.push({
+        content: (function () {
+            var btn_submit = document.createElement("input");
+            btn_submit.className = "btn-submit";
+            btn_submit.setAttribute("type", "submit");
+            btn_submit.setAttribute("value", "Submit");
+            return btn_submit;
+        }) ()
+    });
+}
+
+
+// 生成问题项
+function createQuestionItem(content) {
+    var item = document.createElement("div");
+    var title = document.createElement("div");
+    var options = document.createElement("div");
+    var title_text = document.createElement("p");
+    // var option_ul = document.createElement("ul");
+
+    item.setAttribute("id", content.id);
+
+    item.className = "question_content";
+    title.className = "question_title";
+    options.className = "question_options";
+    // option_ul.className = "option_list";
+
+    title_text.innerHTML = content.content;
+    title.appendChild(title_text);
+    item.appendChild(title);
+
+    for (var k in content.options) {
+        // var li_c = document.createElement("li");
+        var option_line = document.createElement("div");
+        var ratio_input = document.createElement("input");
+        var input_label = document.createElement("label");
+        var br = document.createElement("br");
+
+        option_line.className = "option_line";
+
+        ratio_input.setAttribute("type", content.type);
+        ratio_input.setAttribute("name", content.id);
+        ratio_input.setAttribute("value", k);
+
+        input_label.innerHTML = content.options[k];
+
+        // li_c.appendChild(ratio_input);
+        // option_ul.appendChild(li_c);
+        option_line.appendChild(ratio_input);
+        option_line.appendChild(input_label);
+        options.appendChild(option_line);
+        options.appendChild(br);
     }
-    document.write("<input type=\"submit\" name=\"name\" value=\"Submit\">")
-    // document.write("<input type=\"text\" name=\"name\" value=\"Mickey\">")
-    // document.write("<input type=\"submit\" name=\"name\" value=\"Mickey\">")
+
+    // options.appendChild(option_ul);
+    item.appendChild(options);
+    return item;
 }
