83549dd621dff0bc82d9bed21a36ddfcdd6e21c8
ETZhangSX <etzhangsx@qq.com>
2019-06-18 23:59:25 +0800

    修改分享链接键

--- js/result.js
+++ js/result.js
@@ -178,7 +178,7 @@ function setupWxShare() {
             title: share['title'],
             desc: share['desc'],
             link: share['link'],
-            img_url: share['imgUrl'],
+            imgUrl: share['imgUrl'],
             success: function (res) {
                 console.log("result: wxshare setup success.");
                 console.log(res);
@@ -197,7 +197,7 @@ function setupWxShare() {
         wx.onMenuShareTimeline({
             title: share['title'],
             link: share['link'],
-            img_url: share['imgUrl'],
+            imgUrl: share['imgUrl'],
             success: function (res) {
                 console.log("result: wxshare setup success.");
                 console.log(res);
